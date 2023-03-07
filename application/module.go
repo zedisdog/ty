b008ba66d1d80b55be485a5b79a5dde94c3eb7e3
zed <zedsmail@163.com>
2023-03-07 16:30:48 +0800

    docs

--- application/module.go
+++ application/module.go
@@ -4,6 +4,8 @@ import "github.com/zedisdog/ty/config"
 
 type IModule interface {
 	Name() string
+	// Register registers resource to application. e.g: route used by default http server
 	Register(application IApplication) error
+	// Bootstrap start module's own sub process.
 	Bootstrap(config config.IConfig) error
 }
